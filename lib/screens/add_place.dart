e461aec01834d90a1ad637dce067eb834f0259e5
Stefano Bianda <stefanobianda@ik.me>
2024-06-21 10:44:03 +0200

    add image widget

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -48,6 +49,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onSurface,
               ),
             ),
+            const SizedBox(
+              height: 16,
+            ),
+            const ImageInput(),
             const SizedBox(
               height: 16,
             ),
