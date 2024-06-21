722f6064ede6e77b904b6175841dfaf53e57e600
Stefano Bianda <stefanobianda@ik.me>
2024-06-21 14:02:10 +0200

    add location input widget layout

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -14,8 +14,26 @@ class _LocationInputState extends State<LocationInput> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+            ),
+          ),
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          child: Text(
+            'No location chosen.',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+          ),
+        ),
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
               icon: const Icon(Icons.location_on),
